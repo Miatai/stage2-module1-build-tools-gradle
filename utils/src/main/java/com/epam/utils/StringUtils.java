d02dfb50f5d7fa85cdbd0ed9404baf727cede5d0
Miatai <nullus25@gmail.com>
2023-05-31 19:30:10 +0300

    fix test failure caused by throwing an exception

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,9 +4,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (!NumberUtils.isCreatable(str)) {
-            throw new NumberFormatException();
-        }
-         return NumberUtils.toDouble(str) > 0;
+         return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
